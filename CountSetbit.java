dcf9a2800e1dd254dc95a1797c535ce31edeb29d
Vichu-14 <113173825+Vichu-14@users.noreply.github.com>
2022-09-09 14:40:41 +0530

    Add files via upload

---
+++ CountSetbit.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+
+public class CountSetbit
+{
+	
+
+public static void main(String[] args) {
+	     
+
+Scanner sc=new Scanner(System.in);
+	     
+
+int n=sc.nextInt();
+	     
+
+int a=0;
+	    
+
+while(n>0){
+	        
+
+   if((n&1)==1){
+	            
+   
+           a++;
+	        
+        }
+	        
+
+        n=n>>1;
+	  
+  
+      }
+	    
+
+      System.out.print(a);
+	
+  }
+
+
+}
